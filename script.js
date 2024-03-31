5b0afd71cb93fadc130dc92e3a03e6035834960a
Rangga Mukti <r4n994mukti@gmail.com>
2024-03-31 16:38:20 +0700

    Mengubah Desain Banner

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function on() {
+  var menunBar = document.getElementById('menuBar');
+
+  var overlay = document.getElementById('overlay');
+
+  menunBar.classList.toggle('menuBarStyle');
+  overlay.classList.toggle('overlayStyle');
+}
+
 const rootStyles = document.querySelector(':root');
 function handleWindowResize() {
   const scrollSectionWidth = document.querySelector('.scroll-section').clientWidth;
